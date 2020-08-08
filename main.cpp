b962e725af556e5b03e18caef03d49e140aac89d
Dwight <dduffie@msjc.edu>
2020-08-07 19:05:38 -0700

    This is working now! New Features version 2!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! This is Dwights updated code" << std::endl;
+    std::cout << "Hello, World! This is Dwights updated code - version 2 changes" << std::endl;
     return 0;
 }
